0d530a91ccaec16da291f39a81cf15e751774b0b
abdelrahmanmaher <abdelrahmanmaher1234@gmail.com>
2024-11-13 11:03:18 +0200

    show modal bottom shet

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -5,6 +5,7 @@ import 'package:notes_app/widgets/notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
